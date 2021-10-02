1e21c8423e49bc08815ad4ecfd914bbc96c0d5b9
Andres Benavides <andres.benavides@eventsite.co>
2021-10-02 12:07:24 -0500

    Validar token

--- consulta-citas/main.py
+++ consulta-citas/main.py
@@ -2,7 +2,7 @@ import os
 import firebase_admin
 from flask import Flask, request
 from flask_jwt_extended import JWTManager
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required,get_jwt_identity
 from firebase_admin import credentials
 from firebase_admin import firestore
 import json
@@ -20,20 +20,32 @@ firebase_admin.initialize_app(cred, {
 })
 db = firestore.client()
 
+
+@app.route("/", methods=["GET"])
+def home():
+    return "Microservicio para consultar las citas"
+
 @app.route("/consulta-citas", methods=["GET"])
-#@jwt_required()
-def facturador():
+@jwt_required()
+def consulta():
     id_paciente = request.args.get('id_paciente', type=int)
-    citas_ref = db.collection(u'citas')
-    citas = citas_ref.stream()
-    citasResponse = []
-    for cita in citas:
-       citaData = cita.to_dict()
-       citasResponse.append(citaData)
-    #    if(pacienteData.get('id_paciente') == id_paciente):
-    #        return { "total": random.randint(212,999)  }
-    #return { "total": ((((id_paciente % 9) * 626 + 2) / 9) * 7   )  }
-    return json.dumps(citasResponse)
+
+    jwt = get_jwt_identity()
+    id_paciente = jwt.get("user_id")
+    perfil = jwt.get("profile")
+
+    if perfil == 0:
+        citas_ref = db.collection(u'citas')
+        citas = citas_ref.stream()
+        citasResponse = []
+        for cita in citas:
+            citaData = cita.to_dict()
+            if(citaData.get('id_paciente') == id_paciente):
+                citasResponse.append(citaData)
+
+        
+        return json.dumps(citasResponse)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
